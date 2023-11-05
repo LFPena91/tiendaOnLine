a4ddc06a06b77ac82419c5e33aecaa852f1f787c
LFPena91 <lfpena@soyucn.edu.co>
2023-11-05 08:35:05 -0500

    Se adicionan manejo a base de datos

--- Formulario.php
+++ Formulario.php
@@ -16,13 +16,78 @@
     </header>
     <p>
       <?php
-        $nombre = $_POST['nombre'];
-        $apellidos = $_POST['apellidos'];
-        $correo = $_POST['correo'];
-        $celular = $_POST['celular'];
-        $servicio = $_POST['servicio'];
 
-        echo  
+        //Creo la conexión a la BD
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "tiendaonline";
+        
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Verifica la conexión
+        if ($conn->connect_errno) {
+          echo "Falló la conexión a MySQL: " . $conn->connect_error;
+          exit();
+        }
+
+        //escapamos los datos ingresados al formulario
+        $nombre = $conn->real_escape_string($_POST['nombre']);
+        $apellidos = $conn->real_escape_string($_POST['apellidos']);
+        $correo = $conn->real_escape_string($_POST['correo']);
+        $celular = $conn->real_escape_string($_POST['celular']);
+        $servicio = $conn->real_escape_string($_POST['servicio']);
+
+        //Genero una consulta, para evitar valores repetidos.
+        $consulta = "SELECT estado FROM tiendaonline.clientes
+                    WHERE celular = $celular
+                      AND servicio = '$servicio'";
+        $resultado = mysqli_query($conn,$consulta);
+        //Primero se valida si en la fila estado hay algún dato
+        if ($resultado && mysqli_num_rows($resultado) > 0){
+          //Recorremos cada uno de los datos encontrados
+          while ($fila = mysqli_fetch_assoc($resultado)) {
+            if ($fila['estado'] == "Pendiente"){ 
+              echo 
+              "<div class = 'formulario'>
+                <p><b>Su solicitud ya ha sido creada, pronto nos comunicaremos con usted</b></p>
+                <a href='Ppal.html'>Finalizar</a>
+              </div>";
+            }else{
+              $actualizacion = "UPDATE clientes
+                                SET nombres = '$nombre' , apellidos = '$apellidos' , 
+                                    correo_electronico = '$correo' , servicio = '$servicio' , estado = 'Pendiente'
+                                WHERE celular = $celular AND estado <> 'Pendiente'"; //agregar condicional servicio
+              if (mysqli_query($conn,$actualizacion)){
+                echo 
+                "<div class = 'formulario'>
+                  <p><b>Datos actualizados, pronto nos comunicaremos con usted</b></p>
+                    <ul class = 'pregunta'>
+                      <li>Nombre(s): </li>
+                      <li>Apellido(s): </li>
+                      <li>Correo electrónico: </li>
+                      <li>Celular: </li>
+                      <li>Servicio que solicita: </li>
+                    </ul>
+                    <ul class = 'respuesta'>
+                      <li>". $nombre. "</li>
+                      <li>". $apellidos. "</li>
+                      <li>". $correo. "</li>
+                      <li>". $celular. "</li>
+                      <li>". $servicio. "</li>
+                    </ul>
+                  <a href='Ppal.html'>Finalizar</a>
+                </div>";
+              }else{
+                echo "Error";
+              }
+            }
+          }
+        }else{
+          $query = "INSERT INTO clientes 
+                    (nombres, apellidos, correo_electronico, celular, servicio, estado)
+                    VALUES ('{$nombre}','{$apellidos}','{$correo}','{$celular}','{$servicio}','Pendiente')";
+          $conn->query($query);
+          echo  
           "<div class = 'formulario'>
             <p><b>Esta es la información enviada:</b></p>
               <ul class = 'pregunta'>
@@ -41,8 +106,10 @@
               </ul>
               <a href='Ppal.html'>Finalizar</a>
           </div>";
+        }
+      //se cierra la conexión
+      $conn->close();
       ?>
     </p>
-
   </body>
 </html>
